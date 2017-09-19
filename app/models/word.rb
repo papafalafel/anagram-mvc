b452dc63e5398d13cd73e02a75496c8a197a221f
papafalafel <jeffrodia@gmail.com>
2017-09-18 23:42:13 -0400

    Add before_create to add letters before creation

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,5 @@
 class Word < ActiveRecord::Base
-
+  before_create :add_letters
 
   def self.find_anagrams(string)
     letters = string.split(//)
@@ -58,6 +58,12 @@ def self.valid_input?(input)
   #else
   #  false
   #end
+end
+
+def add_letters
+  characters = self.text.chars
+  alphabetized_characters = characters.sort
+  self.letters = alphabetized_characters.join
 end
 
 end
