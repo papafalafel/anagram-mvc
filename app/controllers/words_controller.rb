c65a3a7c0c03e0c998c2f3456e6ce37f267fcccd
papafalafel <jeffrodia@gmail.com>
2017-09-28 00:36:58 -0400

    adding form to add words

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -5,6 +5,16 @@ get '/words' do
   erb :"/words/index"
 end
 
+get '/words/new' do
+  @word = Word.new
+  erb :"/words/new"
+end
+
+post '/words' do
+  @word = Word.create(text: params[:text])
+  redirect "/words/#{@word.id}"
+end
+
 get '/words/:id' do
   @word = Word.find(params[:id])
   erb :"/words/show"
